bd3c9cb821686350f5c7a78c57d03e46007c8316
root <root@PrestonDesktop.localdomain>
2020-04-22 14:08:47 -0500

    quickselect-menu

--- examples/basic_example/src/basic_example.tsx
+++ examples/basic_example/src/basic_example.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import DateRangePicker from "../src/index.tsx";
+import DateRangePicker from "../../../src/index.tsx";
 
 const app = document.getElementById("app");
 
